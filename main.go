23868d81bbddcee5e135efd742e098e4ac6116f3
Rhyster42 <rhysespich@gmail.com>
2025-10-21 19:56:35 -0500

    moved REPL to repl.go, cleaned up functions to be less verbose

--- main.go
+++ main.go
@@ -1,24 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-
-		fmt.Print("Pokedex > ")
-
-		scanner.Scan()
-
-		line := scanner.Text()
-		line_list := strings.Fields(line)
-		msg := fmt.Sprintf("Your command was: %s", line_list[0])
-		fmt.Println(msg)
-	}
+	startRepl()
 
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		wordList := cleanInput(reader.Text())
+		if len(wordList) == 0 {
+			continue
+		}
+
+		msg := fmt.Sprintf("Your command was: %s", wordList[0])
+		fmt.Println(msg)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
