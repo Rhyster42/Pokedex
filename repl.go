493f89a42297c662a9be0d59d005a3fcf974e92e
Rhyster42 <rhysespich@gmail.com>
2025-10-22 22:21:03 -0500

    built commands map and got basic functions help and exit functioning

--- repl.go
+++ repl.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandMap map[string]cliCommand
+
+func init() {
+	commandMap = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
@@ -19,9 +42,32 @@ func startRepl() {
 			continue
 		}
 
-		msg := fmt.Sprintf("Your command was: %s", wordList[0])
-		fmt.Println(msg)
+		command := wordList[0]
+		if cmd, exists := commandMap[command]; exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+			}
+		} else {
+			fmt.Printf("Unknown command: %s\n", command)
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+
+	for _, command := range commandMap {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
 }
 
 func cleanInput(text string) []string {
