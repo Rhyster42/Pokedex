f8bffc94598a4b0a6573f174d03484fb6882a1fe
Rhyster42 <rhysespich@gmail.com>
2025-11-10 13:49:17 -0600

    rebuilt client structure, removing outside variables from main functions, reorganized code to be more streamlined and readable

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -1,38 +1,22 @@
-package requests
+package pokeapi
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
+	"time"
+
+	"github.com/Rhyster42/Pokedex/internal/pokecache"
 )
 
-func GetData(url string) (mapPage, error) {
+type Client struct {
+	cache      pokecache.Cache
+	httpClient http.Client
+}
 
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+func NewClient(timeout, cacheInterval time.Duration) Client {
+	return Client{
+		cache: pokecache.NewCache(cacheInterval),
+		httpClient: http.Client{
+			Timeout: timeout,
+		},
 	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var data mapPage
-
-	if err := json.Unmarshal(body, &data); err != nil {
-		log.Fatalf("Error unmarshalling JSON: %v", err)
-	}
-
-	for i := 0; i < len(data.Results); i++ {
-		fmt.Println(data.Results[i].Name)
-	}
-
-	return data, nil
 }
