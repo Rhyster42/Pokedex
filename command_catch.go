d3a48ba0c831059fb8fc964a52782a0359ce9359
Rhyster42 <rhysespich@gmail.com>
2025-11-11 16:03:41 -0600

    Built function, still needed to add caught pokemon functionality

---
+++ command_catch.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func commandCatch(cfg *config, pokemon string) error {
+	if pokemon == "" {
+		return errors.New("no pokemon argument, please add pokemon you are attempting to catch")
+	}
+
+	pokemonResp, err := cfg.pokeapiClient.GetPokemon(pokemon)
+	if err != nil {
+		return err
+	}
+
+	// catch Pokemon logic
+	initialMsg := fmt.Sprintf("Throwing a Pokeball at %s...", pokemon)
+	fmt.Println(initialMsg)
+
+	catchChance := int((500 - pokemonResp.BaseExperience) / 5)
+	randomPercent := rand.Intn(100)
+
+	if randomPercent <= catchChance {
+		caughtMsg := fmt.Sprintf("%s was caught!", pokemon)
+		fmt.Println(caughtMsg)
+	} else {
+		caughtMsg := fmt.Sprintf("%s escaped!", pokemon)
+		fmt.Println(caughtMsg)
+	}
+	return nil
+}
