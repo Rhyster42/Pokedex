9f0c2b1d0d292cb3721028b7e4781512be06d957
Rhyster42 <rhysespich@gmail.com>
2025-11-13 16:50:38 -0600

    final update for project submission

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Rhyster42/Pokedex
+
+go 1.25.1
--- internal/pokeapi/list_locations.go
+++ internal/pokeapi/list_locations.go
@@ -12,6 +12,7 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
+	// cache check
 	if val, ok := c.cache.Get(url); ok {
 		locationsResp := RespShallowLocations{}
 		err := json.Unmarshal(val, &locationsResp)
@@ -21,28 +22,33 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		return locationsResp, nil
 	}
 
+	// request
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
 
+	// pull response data from request
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
 	defer resp.Body.Close()
 
+	// read data
 	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
 
+	// unmarshal data from JSON
 	locationsResp := RespShallowLocations{}
 	err = json.Unmarshal(dat, &locationsResp)
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
 
+	//add data to cache, return
 	c.cache.Add(url, dat)
 	return locationsResp, nil
 }
