410d72a7597b12456b020ef2d7fa8be867b10240
Rhyster42 <rhysespich@gmail.com>
2025-11-13 16:03:47 -0600

    found data wasn't saving Pokemon data, cleaned up code in command_catch

--- command_catch.go
+++ command_catch.go
@@ -6,30 +6,29 @@ import (
 	"math/rand"
 )
 
-func commandCatch(cfg *config, pokemon string) error {
-	if pokemon == "" {
+func commandCatch(cfg *config, name string) error {
+	if name == "" {
 		return errors.New("no pokemon argument, please add pokemon you are attempting to catch")
 	}
 
-	pokemonResp, err := cfg.pokeapiClient.GetPokemon(pokemon)
+	pokemon, err := cfg.pokeapiClient.GetPokemon(name)
 	if err != nil {
 		return err
 	}
 
 	// catch Pokemon logic
-	initialMsg := fmt.Sprintf("Throwing a Pokeball at %s...", pokemon)
+	initialMsg := fmt.Sprintf("Throwing a Pokeball at %s...", name)
 	fmt.Println(initialMsg)
 
-	catchChance := int((500 - pokemonResp.BaseExperience) / 5)
+	catchChance := int((500 - pokemon.BaseExperience) / 5)
 	randomPercent := rand.Intn(100)
 
 	if randomPercent <= catchChance {
-		caughtMsg := fmt.Sprintf("%s was caught!", pokemon)
+		caughtMsg := fmt.Sprintf("%s was caught!", pokemon.Name)
 		fmt.Println(caughtMsg)
-		cfg.caughtPokemon[pokemon] = pokemonResp
-		fmt.Println(cfg.caughtPokemon)
+		cfg.caughtPokemon[pokemon.Name] = pokemon
 	} else {
-		caughtMsg := fmt.Sprintf("%s escaped!", pokemon)
+		caughtMsg := fmt.Sprintf("%s escaped!", pokemon.Name)
 		fmt.Println(caughtMsg)
 	}
 	return nil
